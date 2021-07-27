0a9368b09846c6fb56272e870f354f608fb035cb
jdebacker <jason.debacker@gmail.com>
2021-07-27 16:02:19 -0400

    test that output matches expected

---
+++ tests/test_outputs.R
@@ -0,0 +1,15 @@
+library(here)
+
+# get directory for this project (in this case, it's where .git is)
+proj_dir <- here::here()
+
+file_list = list("asset_averages", "cit_rates_timeseries", "eu_cctb",
+                 "npv_europe", "npv_ranks_2020",
+                 "npv_weighted_timeseries")
+for (file in file_list) {
+  test <- read.delim(file.path(proj_dir, "final-outputs", paste(file, ".csv", sep="")), sep=",", fill = TRUE)
+  expected <- read.delim(file.path(proj_dir, "final-outputs-expected", paste(file, ".csv", sep="")), sep=",", fill = TRUE)
+  test_that(paste("Test", file, "results"),{
+  expect_equal(test, expected)
+  })
+}
