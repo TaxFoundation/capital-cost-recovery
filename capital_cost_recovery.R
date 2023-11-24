66dc3475934a320559c480229504a449fd3691b1
Alex Mengden <amengden@taxfoundation.org>
2023-11-24 16:32:56 +0100

    Forecast through 2027
    Key updates: | - UK makes full expensing permanent | - Germany re-enacts DB depreciation throughout 2024

--- capital_cost_recovery.R
+++ capital_cost_recovery.R
@@ -374,12 +374,11 @@ data[c('machines_cost_recovery')][data$country == "USA" & data$year == 2024,] <-
 data[c('machines_cost_recovery')][data$country == "USA" & data$year == 2025,] <- (data[c('machines_cost_recovery')][data$country == "USA" & data$year == 2025,] * 0.60) + 0.40
 data[c('machines_cost_recovery')][data$country == "USA" & data$year == 2026,] <- (data[c('machines_cost_recovery')][data$country == "USA" & data$year == 2026,] * 0.80) + 0.20
                                                                                             
-#Ajdust UK data to include super-deduction
+#Ajdust UK data to include super-deduction and permanent full expensing
 data[c('machines_cost_recovery')][data$country == "GBR" & data$year == 2021,] <- (data[c('machines_cost_recovery')][data$country == "GBR" & data$year == 2021,] * 0.00) + 1.30
 data[c('machines_cost_recovery')][data$country == "GBR" & data$year == 2022,] <- (data[c('machines_cost_recovery')][data$country == "GBR" & data$year == 2022,] * 0.00) + 1.30
-data[c('machines_cost_recovery')][data$country == "GBR" & data$year == 2023,] <- 1
-data[c('machines_cost_recovery')][data$country == "GBR" & data$year == 2024,] <- 1
-data[c('machines_cost_recovery')][data$country == "GBR" & data$year == 2025,] <- 1
+data[c('machines_cost_recovery')][data$country == "GBR" & data$year >= 2023,] <- 1
+
                                                                                             
 #Only keep columns with the calculated net present values
 data <- subset(data, select = c(country, year, buildings_cost_recovery, machines_cost_recovery, intangibles_cost_recovery))
