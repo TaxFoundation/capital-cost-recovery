39fa2623b9d83059ab5c477f63c28cda3eff6587
jdebacker <jason.debacker@gmail.com>
2024-03-22 21:00:24 -0400

    break up for clarity

--- tests/test_outputs.R
+++ tests/test_outputs.R
@@ -8,8 +8,10 @@ file_list = list("asset_averages", "cit_rates_timeseries", "eu_cctb",
                  "npv_europe", "npv_ranks_2022",
                  "npv_weighted_timeseries")
 for (file in file_list) {
-  test <- read.delim(file.path(proj_dir, "final-outputs", paste(file, ".csv", sep="")), sep=",", fill = TRUE)
-  expected <- read.delim(file.path(proj_dir, "final-outputs-expected", paste(file, ".csv", sep="")), sep=",", fill = TRUE)
+  file_test <- file.path(proj_dir, "final-outputs", paste(file, ".csv", sep=""))
+  test <- read.delim(file_test, sep=",", fill = TRUE)
+  file_expected <- file.path(proj_dir, "final-outputs-expected", paste(file, ".csv", sep=""))
+  expected <- read.delim(file_expected, sep=",", fill = TRUE)
   test_that(paste("Test", file, "results"),{
   expect_equal(test, expected)
   })
@@ -17,7 +19,7 @@ for (file in file_list) {
 
 # Test file in the final-data directory
 test_that(paste("Test final-data results"),{
-    test <- read.delim(file.path(proj_dir, "final-data", paste("npv_all_years", ".csv", sep="")), sep=",", fill = TRUE)
+    test <- read.delim(subfile.path(proj_dir, "final-data", paste("npv_all_years", ".csv", sep="")), sep=",", fill = TRUE)
     expected <- read.delim(file.path(proj_dir, "final-data-expected", paste("npv_all_years", ".csv", sep="")), sep=",", fill = TRUE)
     expect_equal(test, expected)
   })
